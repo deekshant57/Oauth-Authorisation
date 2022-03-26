d5ece4689c7e3378057fcdefd44655cf7b39693c
deekshant57 <80623479+deekshant57@users.noreply.github.com>
2022-03-27 00:44:16 +0530

    Update db.js

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,13 +1,9 @@
 const mongoose = require("mongoose");
 
-// module.exports = () => {
-//   return mongoose.connect(
-//     "mongodb://127.0.0.1:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
-//   );
-// };
-
 module.exports = () => {
   return mongoose.connect(
-    "mongodb+srv://deekshant57:deekshant_123@practice.9w926.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    "mongodb://127.0.0.1:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
   );
 };
+
+
